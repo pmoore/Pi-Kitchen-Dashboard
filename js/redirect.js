992529baa6ecc3c8901094bf3df52266d5b3e2d9
pmoore <pmmoor1@gmail.com>
2016-07-15 11:05:20 -0400

    redirect

--- js/redirect.js
+++ js/redirect.js
@@ -1,11 +1,9 @@
 // Redirect the loaded page to the selected skin's index
 
 (function() {
-	// var href = window.location.href;
-	// var index = href.indexOf('index.html');
-	// href = href.slice(0, index);
+	var href = window.location.href;
+	var index = href.indexOf('index.html');
+	href = href.slice(0, index);
 
-	// window.location = href + 'skins/' + skinName + '/index.html';
-
-	window.location = '/skins/' + skinName + '/index.html';
+	window.location = href + 'skins/' + skinName + '/index.html';
 }());
